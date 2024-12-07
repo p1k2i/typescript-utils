ee65c6a9aef3e477141008fd7c7794bea2bdb0cb
p1k2i <pro100kryto.original@gmail.com>
2024-12-07 21:45:30 +0000

    Add number-as-string.ts

---
+++ number-as-string.ts
@@ -0,0 +1,5 @@
+export type NumberAsString = `${number}`;
+
+export type PositiveNumberAsString = `${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}${number | ''}`;
+
+export type NegativeNumberAsString = `-${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}${number | ''}`;
