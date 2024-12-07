1b969bca298cab88dc03f5268d21e4c62ee4c617
p1k2i <pro100kryto.original@gmail.com>
2024-12-07 22:03:23 +0000

    Format ipv6-to-binary.ts

--- ipv6-to-binary.ts
+++ ipv6-to-binary.ts
@@ -1,20 +1,20 @@
 export function binaryToIPv6(binary: Buffer): string {
-    const address = binary.toString("hex");
-    const chunks = address.match(/.{1,4}/g) || [];
-    return chunks.join(":");
+  const address = binary.toString("hex");
+  const chunks = address.match(/.{1,4}/g) || [];
+  return chunks.join(":");
 }
 
 export function iPv6ToBinary(ipAddress: string): Buffer {
-    const chunks = ipAddress.split(":");
-    const buffer = Buffer.alloc(16);
-  
-    let offset = 0;
-    for (let i = 0; i < chunks.length; i++) {
-      const chunk = chunks[i];
-      const chunkValue = parseInt(chunk, 16);
-      buffer.writeUInt16BE(chunkValue, offset);
-      offset += 2;
-    }
-  
-    return buffer;
+  const chunks = ipAddress.split(":");
+  const buffer = Buffer.alloc(16);
+
+  let offset = 0;
+  for (let i = 0; i < chunks.length; i++) {
+    const chunk = chunks[i];
+    const chunkValue = parseInt(chunk, 16);
+    buffer.writeUInt16BE(chunkValue, offset);
+    offset += 2;
+  }
+
+  return buffer;
 }
